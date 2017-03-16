c5047af46830f9239a6deb8e7807fe5014ac422d
liujunshi <524607630@qq.com>
2017-03-16 12:57:40 +0800

    创建树的逻辑

--- MyLZSS/LZSS.cpp
+++ MyLZSS/LZSS.cpp
@@ -15,13 +15,12 @@ using namespace std;
 
 LZSS::LZSS()
 {
-    for (int32_t i = 0; i < ROOT_SIZE; ++i) {
-        m_root[i] = EMPTY_NODE;
+    for (int32_t i = 0 ; i < LZ_WINDOW_SIZE + ROOT_SIZE; ++i){
+        m_window[i].lChild = NO_MATCH;
+        m_window[i].rChild = NO_MATCH;
+        m_window[i].dad = NO_MATCH;
     }
-    for (int32_t i = 0 ; i < LZ_WINDOW_SIZE; ++i){
-        m_window[i].lChild = EMPTY_NODE;
-        m_window[i].rChild = EMPTY_NODE;
-    }
+    m_root = &m_window[LZ_WINDOW_SIZE];
     m_CurPos = 0;
     m_IsFill = false;
 }
@@ -30,73 +29,112 @@ uint32_t LZSS::CalculateHash(const uint8_t* inputCursor){
     return uint32_t(inputCursor[0]);// + uint32_t(inputCursor[1]<<8);
 }
 
+//将节点treeCursor从树中删除
+void LZSS::DeleteNode(int32_t deleteCursor)
+{
+    int32_t replaceCursor = 0;//替换的
+    
+    if (m_window[deleteCursor].dad == NO_MATCH) {
+        return;
+    }
+    
+    if (m_window[deleteCursor].rChild == NO_MATCH) {      //被删除的节点只有左孩子，找到
+        replaceCursor = m_window[deleteCursor].lChild;
+    }else if (m_window[deleteCursor].lChild == NO_MATCH){ //被删除的节点只有右孩子
+        replaceCursor = m_window[deleteCursor].rChild;
+    }else{ //找到被删除的节点左节点的最右子节点，作为被删除节点的替换
+        replaceCursor = m_window[deleteCursor].lChild;  //找到左孩子
+        if (m_window[deleteCursor].rChild != NO_MATCH) {
+            do {
+                replaceCursor = m_window[replaceCursor].rChild;
+            } while (m_window[replaceCursor].rChild != NO_MATCH);
+            
+            if (m_window[replaceCursor].lChild != NO_MATCH) {
+                m_window[m_window[replaceCursor].dad].rChild = m_window[replaceCursor].lChild;
+                m_window[m_window[replaceCursor].lChild].dad = m_window[replaceCursor].dad;
+            }
+            m_window[replaceCursor].lChild = m_window[deleteCursor].lChild;
+            m_window[m_window[deleteCursor].lChild].dad = replaceCursor;
+        }
+        m_window[replaceCursor].rChild = m_window[deleteCursor].rChild;
+        m_window[m_window[deleteCursor].rChild].dad = replaceCursor;
+    }
+    
+    //更新替换节点的父节点为被删除节点的父节点
+    m_window[replaceCursor].dad = m_window[deleteCursor].dad;
+    
+    //将父节点的子节点设置为要替换的节点
+    if (m_window[m_window[deleteCursor].dad].rChild == deleteCursor) {//被删除的节点是右子节点
+        m_window[m_window[deleteCursor].dad].rChild = replaceCursor;  //将右子节点设为替换的节点
+    }else{
+        m_window[m_window[deleteCursor].dad].lChild = replaceCursor;
+    }
+    
+    //删除当前节点
+    m_window[deleteCursor].dad = NO_MATCH;
+}
+
 int32_t LZSS::InsertNode(int32_t windowCursor,const uint8_t *inputChar,int32_t inputLength)
 {
     const uint8_t* inputCursor = (uint8_t*)inputChar;
     int32_t cmp = 1;
     uint32_t hash =  CalculateHash(inputCursor);
-    int32_t treeCursor = m_root[hash];
+    int32_t treeCursor = m_window[hash + LZ_WINDOW_SIZE].rChild;
     int32_t matchLength = 0;
     
+    if (treeCursor == NO_MATCH) {
+        m_window[hash + LZ_WINDOW_SIZE].rChild = windowCursor;
+        m_window[windowCursor].lChild = NO_MATCH;
+        m_window[windowCursor].rChild = NO_MATCH;
+        m_window[windowCursor].dad = hash + LZ_WINDOW_SIZE;
+        return matchLength;
+    }
+    
     for (; ;) {
+        //int32_t maxMatchLength = windowCursor - treeCursor;
+        //maxMatchLength =  maxMatchLength < MAX_MATCH_COUNT ? maxMatchLength:MAX_MATCH_COUNT;
+        int32_t maxMatchLength = MAX_MATCH_COUNT;
+        matchLength = 1;
         
-        if (treeCursor == NO_MATCH) {
-            
-            m_root[hash] = windowCursor;
-            m_window[windowCursor].lChild = NO_MATCH;
-            m_window[windowCursor].rChild = NO_MATCH;
-            break;
-
-        }else{
-            
-            //int32_t maxMatchLength = windowCursor - treeCursor;
-            //maxMatchLength =  maxMatchLength < MAX_MATCH_COUNT ? maxMatchLength:MAX_MATCH_COUNT;
-            int32_t maxMatchLength = MAX_MATCH_COUNT;
-            matchLength = 1;
-            
-            do
-            {
-                cmp = inputCursor[matchLength] - m_Buff[(treeCursor + matchLength)%sizeof(m_Buff)];
-                if(cmp == 0 ){
-                    matchLength++;
-                }
-            }while(cmp == 0 && matchLength < maxMatchLength);
-
-            if (cmp > 0)
-            {
-                if (m_window[treeCursor].rChild == NO_MATCH)
-                {
-                    m_window[treeCursor].rChild = windowCursor;
-                    m_window[windowCursor].dad = treeCursor;
-                    break;
-                }
-                else
-                {
-                    treeCursor = m_window[treeCursor].rChild;
-                    continue;
-                }
+        do{
+            cmp = inputCursor[matchLength] - m_Buff[(treeCursor + matchLength)%sizeof(m_Buff)];
+            if(cmp == 0 ){
+                matchLength++;
             }
-            else if(cmp < 0)
-            {
-                if (m_window[treeCursor].lChild == NO_MATCH)
-                {
-                    m_window[treeCursor].lChild = windowCursor;
-                    m_window[windowCursor].dad = treeCursor;
-                    break;
-                }
-                else
-                {
-                    treeCursor = m_window[treeCursor].lChild;
-                    continue;
-                }
-            }
-            else //matchLength > MAX_MATCH_COUNT
-            {
-                m_window[windowCursor].rChild = m_window[treeCursor].rChild;
-                m_window[windowCursor].lChild = m_window[treeCursor].lChild;
-                
+        }while(cmp == 0 && matchLength < maxMatchLength);
+        
+        if (cmp > 0){
+            if (m_window[treeCursor].rChild == NO_MATCH){
+                m_window[treeCursor].rChild = windowCursor;
+                m_window[windowCursor].dad = treeCursor;
                 break;
+            }else{
+                treeCursor = m_window[treeCursor].rChild;
             }
+        }else if(cmp < 0){
+            if (m_window[treeCursor].lChild == NO_MATCH){
+                m_window[treeCursor].lChild = windowCursor;
+                m_window[windowCursor].dad = treeCursor;
+                break;
+            }else{
+                treeCursor = m_window[treeCursor].lChild;
+            }
+        }else{//matchLength > maxMatchLength 替换节点
+            m_window[windowCursor].dad = m_window[treeCursor].dad;
+            m_window[windowCursor].rChild = m_window[treeCursor].rChild;
+            m_window[windowCursor].lChild = m_window[treeCursor].lChild;
+            
+            m_window[m_window[treeCursor].lChild].dad = windowCursor;
+            m_window[m_window[treeCursor].rChild].dad = windowCursor;
+            
+            if (m_window[m_window[treeCursor].dad].lChild == treeCursor) {
+                m_window[m_window[treeCursor].dad].lChild = windowCursor;
+            }else{
+                m_window[m_window[treeCursor].dad].rChild = windowCursor;
+            }
+            
+            m_window[treeCursor].dad = NO_MATCH;
+            break;
         }
     }
     return matchLength;
--- MyLZSS/LZSS.h
+++ MyLZSS/LZSS.h
@@ -14,9 +14,9 @@ namespace
 {
     const int32_t  NO_MATCH                = -1;
     const int32_t  EMPTY_NODE              = -1;
-    const uint32_t WINDOW_BITS             = 16;
-    const uint32_t MAX_MATCH_COUNT         = 18;
-    const uint32_t MIN_MATCH_COUNT         = 3;
+    const uint32_t WINDOW_BITS             = 8;
+    const uint32_t MAX_MATCH_COUNT         = 4;
+    const uint32_t MIN_MATCH_COUNT         = 2;
     const uint32_t LZ_WINDOW_SIZE          = 1 << WINDOW_BITS;  //2^16
     const uint32_t LZ_WINDOW_MASK          = LZ_WINDOW_SIZE - 1;
     const int32_t  ROOT_SIZE               = LZ_WINDOW_SIZE;
@@ -36,12 +36,12 @@ public:
     void Compress(const void *inputChar,int32_t inputLength,void* outputChar, size_t outputLength);
     void DeCompress();
 protected:
+    void DeleteNode(int32_t deleteCursor);
     int32_t InsertNode(int32_t windowCursor,const uint8_t *inputChar,int32_t inputLength);
     uint32_t CalculateHash(const uint8_t* inputCursor);
 private:
-    int32_t  m_root[ROOT_SIZE];
-    TreeNode m_window[LZ_WINDOW_SIZE];
-    
+    TreeNode m_window[LZ_WINDOW_SIZE + ROOT_SIZE];
+    TreeNode *m_root;
     char m_Buff[LZ_WINDOW_SIZE];
     int32_t m_CurPos;
     bool m_IsFill;
